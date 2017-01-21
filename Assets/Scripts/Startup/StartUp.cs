48d6182ded02339e89b0d886ad962c269ad16983
Tushar Sankhala <tusharsankhala@gmail.com>
2017-01-21 22:41:06 +0530

    Updated the wheel animation, Startup scene and loading the Main game scene.
    Updated the wheel animation, Startup scene and loading the Main game
    scene.

---
+++ Assets/Scripts/Startup/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartUp : MonoBehaviour 
+{
+
+
+	// Loading the main Scene.
+	public void LoadMainScene () 
+	{
+		SceneManager.LoadScene ("SineWaveScene", LoadSceneMode.Single);			
+	}	
+}
